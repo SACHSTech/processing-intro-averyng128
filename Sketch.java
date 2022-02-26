3108722c222a4cd8744ddc00247ab757bb347e5b
averyng128 <chingyan.ng23@ycdsbk12.ca>
2022-02-26 02:42:33 -0500

    resize

--- Sketch.java
+++ Sketch.java
@@ -1,9 +1,14 @@
+/**
+ * A simple program to draw a house
+ * @author: A. Ng
+ * 
+ */
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
   public void settings() {
-	// put your size call here
+	  // put your size call here
     size(400, 400);
   }
 
@@ -13,21 +18,21 @@ public class Sketch extends PApplet {
     
   public void draw() {
 	
-  // foreground
+    // foreground
     fill(90, 140, 90);
     rect(0, 300, 400, 100);
 
-	//house
+	  // house
     fill(200, 170, 150);
-    rect(100,250,200,300);
+    rect(100, 250, 200, 300);
     stroke(255);
-    line(200,150,300,250);
+    line(200, 150, 300, 250);
     stroke(255);
-    line(200,150,100,250);
-    fill(220,170,150);
+    line(200, 150, 100, 250);
+    fill(220, 170, 150);
     triangle(200, 150, 300, 250, 100, 250);
   
-  //sun
+    // sun
     fill(300, 400, 200);
     ellipse(380, 20, 60, 60);
     stroke(225);
@@ -37,13 +42,13 @@ public class Sketch extends PApplet {
     line(355, 53, 350, 70);
     line(375, 60, 380, 80);
 
-  //door
-    fill(220,170,150);
-    rect(200,300, 50, 100);
+    // door
+    fill(220, 170, 150);
+    rect(200, 300, 50, 100);
     fill(200, 170, 150);
     ellipse(240, 350, 7, 7);
 
-  //cloud
+   // cloud
    fill(255);
    ellipse(100, 50, 20, 20);
    ellipse(115, 40, 20, 20);
